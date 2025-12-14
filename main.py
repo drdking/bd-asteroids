dc2e7fe08b48daf5f6b7d8e4ab9b8016ee7694c2
drdking <johnbaranoski@gmail.com>
2025-12-14 15:12:06 -0500

    CH3 Lession 1

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import pygame
-from constants import *
+#from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
-pygame.init()
-
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+
+    game_clock = pygame.time.Clock()        # Sets up Game clock and Delta time variables/functions
+    dt = 0
+
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    #screen_size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 
     while True:
         log_state()
@@ -20,6 +27,10 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
+        # print(dt)                      # Pauses the game until 1/60th of a second passes
+
 
 
 

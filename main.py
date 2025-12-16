bf775f393b434389f22bc5071a7bee1908a9e295
drdking <johnbaranoski@gmail.com>
2025-12-16 13:58:16 -0500

    added asteroid field

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import pygame
 from constants import *
 from logger import log_state
 from player import *
+from asteroid import *
+from asteroidfield import *
+
+
+
 
 
 def main():
@@ -15,10 +20,23 @@ def main():
     game_clock = pygame.time.Clock()        # Sets up Game clock and Delta time variables/functions
     dt = 0
 
-
+    #Defines the screen size
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
+
+    # Creating Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable) # adds player class to group
+
+    Asteroid.containers = (updatable, drawable, asteroids)
+
+    AsteroidField.containers = (updatable)
+    #Creates the player ship
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PALYER_RADIUS)
+
+    a_field = AsteroidField()
     
 
     while True:
@@ -28,14 +46,22 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
+        #player.update(dt) -- Changed to using Groups
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        #player.draw(screen) Updated to containers
+        #Iterating over drawable container to draw objects
+        for ob in drawable:
+            ob.draw(screen)
+
+
+
         pygame.display.flip()
 
         dt = game_clock.tick(60) / 1000
-        # print(dt)                      # Pauses the game until 1/60th of a second passes
+        # print(dt)    Pauses the game until 1/60th of a second passes
 
 
 
--- player.py
+++ player.py
@@ -32,13 +32,10 @@ class Player (CircleShape):
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
-                # ?
         if keys[pygame.K_d]:
-            self.rotate(dt)
-        
+            self.rotate(dt)        
         if keys[pygame.K_w]:
-            self.move(dt)
-        
+            self.move(dt)        
         if keys[pygame.K_s]:
             self.move(-dt)
 
